43f6eac7ba4da64691407a9539d4fd4be693b007
Harminder virk <virk.officials@gmail.com>
2019-05-23 20:07:45 +0530

    feat: implement ioc container

---
+++ index.ts
@@ -0,0 +1,17 @@
+/**
+ * @module main
+ */
+
+/*
+* @adonisjs/fold
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+export { Ioc } from './src/Ioc'
+export { Registrar } from './src/Registrar'
+export { IocContract } from './src/Contracts'
+export { inject } from './src/decorators'
